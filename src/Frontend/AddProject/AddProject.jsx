b0b3245e3e5c8b19f68091da2c4beba8dfc41cb9
Kalina <k.hajzner1@ncl.ac.uk>
2024-03-09 22:45:45 +0000

    Form updates

--- src/Frontend/AddProject/AddProject.jsx
+++ src/Frontend/AddProject/AddProject.jsx
@@ -3,20 +3,18 @@ import { useForm } from "react-hook-form"
 
 const AddProject = () => {
   const {
+    register,
     handleSubmit,
     formState: { errors },
   } = useForm()
 
   const onSubmit = async data => {
-      const response = await fetch('http://localhost:5000/api', {
-        method: 'post',
-        mode: 'cors',
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json'}
-      });
-      const data2 = await response.json();
-      console.log(data2);
-        
+    const response = await fetch('http://localhost:5000/api', {
+      method: 'post',
+      mode: 'cors',
+      body: JSON.stringify(data),
+      headers: {'Content-Type': 'application/json'}
+    });
   }
 
   return (
@@ -24,9 +22,10 @@ const AddProject = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <label>Title</label>
-      <input placeholder="Title" />
+      <input placeholder="Title" {...register("title")}/>
       <label>Description</label>
-      <input placeholder="Description" />
+      <input placeholder="Description" {...register("description")}/>
+
       {errors.exampleRequired && <span>This field is required</span>}
 
       <input type="submit" />
